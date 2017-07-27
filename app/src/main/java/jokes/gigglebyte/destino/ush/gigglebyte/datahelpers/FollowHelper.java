2cfa67dff00a06e4bd0b11eae76d62a89d677dec
Ushal Naidoo <ushal.naidoo@nzdevushaln2.kiwiplan.co.nz>
2017-07-28 09:50:21 +1200

    Nullpointer when followers empty

--- app/src/main/java/jokes/gigglebyte/destino/ush/gigglebyte/datahelpers/FollowHelper.java
+++ app/src/main/java/jokes/gigglebyte/destino/ush/gigglebyte/datahelpers/FollowHelper.java
@@ -19,16 +19,15 @@ public class FollowHelper {
   private static List<User> followers;
 
   public static void initialiseUserFollowing(String users) {
-    users = "{\"users\":" + users + "}";
-    following = JsonParser.GetUsers(users);
+    following = JsonParser.GetUsers("{\"users\":" + users + "}");
   }
 
   public static void initialiseUserFollowers(String users) {
-    users = "{\"users\":" + users + "}";
-    followers = JsonParser.GetUsers(users);
+    followers = JsonParser.GetUsers("{\"users\":" + users + "}");
   }
 
   public static boolean isFollowingUser(int userId) {
+    if (following == null) return false;
     for (User user : following) {
       if (userId == user.getId()) {
         return true;
@@ -38,6 +37,7 @@ public class FollowHelper {
   }
 
   public static void followUser(final Activity activity, final User followUser) {
+    if (following == null) return;
     following.add(followUser);
     final int id = UserHelper.getUsersId(activity);
 
@@ -63,6 +63,7 @@ public class FollowHelper {
   }
 
   public static void unfollowUser(Activity activity, final User unfollowUser) {
+    if (following == null) return;
     for (Iterator<User> iterator = following.listIterator(); iterator.hasNext(); ) {
       User user = iterator.next();
       if (unfollowUser.getId() == user.getId()) {
@@ -111,6 +112,7 @@ public class FollowHelper {
   }
 
   public static List<String> getFollowerNames() {
+    if (followers == null) return new ArrayList<>();
     List<String> names = new ArrayList<>();
     for (User user : followers) {
       if (user.getName() != null && !user.getName().isEmpty()) {
